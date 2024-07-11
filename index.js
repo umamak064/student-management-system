1d8244b86e2525571ecb163d21e0c1c17fc6f795
umamak064 <umamak064@gmail.com>
2024-07-10 20:12:17 -0700

    push student management system on github

--- index.js
+++ index.js
@@ -1,28 +1,79 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+const randomNumber = Math.floor(1000 + Math.random() * 9000);
+let myBalance = 0;
+const useranswer = await inquirer.prompt([
     {
-        message: "Select operator to perform operation",
-        type: "list",
-        name: "operator",
-        choices: ["addition", "multiplication", "subtraction", "division"],
+        type: 'input',
+        message: 'Enter your name:',
+        name: 'students',
+        validate: function (value) {
+            if (value.trim() !== "") {
+                return true;
+            }
+            return "please enter some value";
+        },
+    },
+    {
+        type: 'list',
+        name: 'course',
+        message: 'select course to enroll:',
+        choices: ["MS office", "C++ programming", "web development", "ethical hacking", "cloud computing"]
+    }
+]);
+const tutionfee = {
+    "MS office": 3000,
+    "C++ programming": 4000,
+    "web development": 4500,
+    "ethical hacking": 5000,
+    "cloud computing": 3500
+};
+console.log(`\nTution fees: ${tutionfee[useranswer.course]}/-\n`);
+console.log(`balance ${myBalance}\n`);
+const paymentType = await inquirer.prompt([
+    {
+        type: 'list',
+        name: 'payment',
+        message: 'select payment method:',
+        choices: ["Paypal", "Bank transfer", "Jazz cash", "Easy Paisa"]
+    },
+    {
+        type: 'input',
+        message: 'Enter your amount:',
+        name: 'Tranfer_amount',
+        validate: function (value) {
+            if (value.trim() !== "") {
+                return true;
+            }
+            return "please enter some value";
+        },
     },
 ]);
-//conditional statement
-if (answer.operator === "addition") {
-    console.log(answer.firstNumber + answer.secondNumber);
-}
-else if (answer.operator === "subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
-}
-else if (answer.operator === "multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
-}
-else if (answer.operator === "division") {
-    console.log(answer.firstNumber / answer.secondNumber);
+console.log(`\n You select payment method: ${paymentType.payment}`);
+let Tutionfees = tutionfee[useranswer.course];
+const paymentAmount = parseFloat(paymentType[`Tranfer_amount`]);
+if (Tutionfees === paymentAmount) {
+    console.log(`successfully enrolled in ${useranswer.course}\n`);
+    let ans = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'what would you like to next:',
+            name: 'select',
+            choices: ["view status", "exit"]
+        },
+    ]);
+    if (ans.select === "view status") {
+        console.log("\n **********status**********");
+        console.log(`Student Name: ${useranswer.students}`);
+        console.log(`Student ID ${randomNumber}`);
+        console.log(`course: ${useranswer.course}`);
+        console.log(`Tution fees paid: ${paymentAmount}`);
+        console.log(`Balance: ${myBalance += paymentAmount}`);
+    }
+    else {
+        console.log("\n Exiting Student Management system");
+    }
 }
 else {
-    console.log("please select valid operator");
+    console.log("invalid amount due to course");
 }
--- index.ts
+++ index.ts
@@ -1,29 +1,88 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 
-const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
-  {
-    message: "Select operator to perform operation",
-    type: "list",
-    name: "operator",
-    choices: ["addition", "multiplication", "subtraction","division"],
-  },
-]);
-//conditional statement
-if(answer.operator==="addition"){
-    console.log(answer.firstNumber + answer.secondNumber)
-} 
-else if(answer.operator==="subtraction"){
-    console.log(answer.firstNumber - answer.secondNumber)
+const randomNumber:number=Math.floor(1000 + Math.random() *9000);
+
+let myBalance: number =0;
+
+const useranswer = await inquirer.prompt([
+    {
+        type: 'input',
+        message: 'Enter your name:',
+        name: 'students',
+        validate: function (value) {
+          if (value.trim() !== "") {
+            return true;
+          }
+          return "please enter some value";
+        },
+      },
+      {
+        type: 'list',
+        name: 'course',
+        message: 'select course to enroll:',
+        choices: ["MS office", "C++ programming", "web development", "ethical hacking", "cloud computing"]
+      }
+    ]);
+  const tutionfee:{[key: string]:number} = {
+      "MS office":3000,
+      "C++ programming":4000,
+      "web development":4500,
+      "ethical hacking":5000,
+      "cloud computing":3500
+  }
+  console.log(`\nTution fees: ${tutionfee[useranswer.course]}/-\n`);
+  console.log(`balance ${myBalance}\n`);
+
+  const paymentType = await inquirer.prompt([
+    {
+        type: 'list',
+        name: 'payment',
+        message: 'select payment method:',
+        choices: ["Paypal", "Bank transfer", "Jazz cash", "Easy Paisa"]
+    },
+    {
+        type: 'input',
+        message: 'Enter your amount:',
+        name: 'Tranfer_amount', 
+        validate: function (value) {
+          if (value.trim() !== "") {
+            return true;
+          }
+          return "please enter some value";
+        },
+      },
+
+  ]);
+  console.log(`\n You select payment method: ${paymentType.payment}`);
+let  Tutionfees= tutionfee [useranswer.course];
+const paymentAmount = parseFloat(paymentType[`Tranfer_amount`]);
+
+if (Tutionfees=== paymentAmount){
+    console.log(`successfully enrolled in ${useranswer.course}\n`);
+
+    let ans = await inquirer.prompt([
+       {
+        type: 'list',
+        message: 'what would you like to next:',
+        name: 'select', 
+        choices:["view status", "exit"]
+       },
+
+])
+if (ans.select==="view status"){
+    console.log("\n **********status**********");     
+    console.log(`Student Name: ${useranswer.students}`);
+    console.log(`Student ID ${randomNumber}`);
+    console.log(`course: ${useranswer.course}`);
+    console.log(`Tution fees paid: ${paymentAmount}`);
+    console.log(`Balance: ${myBalance += paymentAmount}`);
+  
+}else{ 
+    console.log("\n ------------ Exiting Student Management system-------------");
 }
-else if(answer.operator==="multiplication"){
-    console.log(answer.firstNumber * answer.secondNumber)
-}
-else if(answer.operator==="division"){
-    console.log(answer.firstNumber / answer.secondNumber)
+
 }
 else {
-    console.log("please select valid operator")
+    console.log("invalid amount due to course")
 }
